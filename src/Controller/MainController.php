b6715245d56df96716ec591fa3735fb45adc9035
bhhdev <bhh.develop@gmail.com>
2025-03-18 11:20:26 +0100

    chore: Corrections error500, H1-pages-twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -17,30 +17,30 @@ final class MainController extends AbstractController
     #[Route('/dashboard', name: 'app_dashboard')]
     public function dashboard(): Response
     {
-        return $this->render('dashboard/dashboard.html.twig');
+        return $this->render('nav/dashboard.html.twig');
     }
 
     #[Route('/create_quiz', name: 'app_create_quiz')]
     public function createQuiz(): Response
     {
-        return $this->render('create_quiz/create_quiz.html.twig');
+        return $this->render('nav/create_quiz.html.twig');
     }
 
     #[Route('/waiting_room', name: 'app_waiting_room')]
     public function waittingRoom(): Response
     {
-        return $this->render('waiting_room/waiting_room.html.twig');
+        return $this->render('nav/waiting_room.html.twig');
     }
 
     #[Route('/quiz_in_progress', name: 'app_quiz_in_progress')]
     public function quizInProgress(): Response
     {
-        return $this->render('quiz_in_progress/quiz_in_progress.html.twig');
+        return $this->render('nav/quiz_in_progress.html.twig');
     }
 
     #[Route('/game_result', name: 'app_game_result')]
     public function gameResult(): Response
     {
-        return $this->render('game_result/game_result.html.twig');
+        return $this->render('nav/game_result.html.twig');
     }
 }
