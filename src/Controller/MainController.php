7d36bd958debf5208932452f7683cb9a6971d02d
nicov51 <nicolas.viennot@laposte.net>
2025-03-19 16:01:42 +0100

    feat: add partials,implements basehtml and homepage
    add link bulma and extends base of all pages

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,36 +11,50 @@ final class MainController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('nav/index.html.twig');
+        return $this->render('pages/index.html.twig');
     }
 
     #[Route('/dashboard', name: 'app_dashboard')]
     public function dashboard(): Response
     {
-        return $this->render('nav/dashboard.html.twig');
+        return $this->render('pages/dashboard.html.twig');
     }
 
     #[Route('/create_quiz', name: 'app_create_quiz')]
     public function createQuiz(): Response
     {
-        return $this->render('nav/create_quiz.html.twig');
+        return $this->render('pages/create_quiz.html.twig');
     }
 
     #[Route('/waiting_room', name: 'app_waiting_room')]
-    public function waittingRoom(): Response
+    public function waitingRoom(): Response
     {
-        return $this->render('nav/waiting_room.html.twig');
+        return $this->render('pages/waiting_room.html.twig');
     }
 
     #[Route('/quiz_in_progress', name: 'app_quiz_in_progress')]
     public function quizInProgress(): Response
     {
-        return $this->render('nav/quiz_in_progress.html.twig');
+        return $this->render('pages/quiz_in_progress.html.twig');
     }
 
     #[Route('/game_result', name: 'app_game_result')]
     public function gameResult(): Response
     {
-        return $this->render('nav/game_result.html.twig');
+        return $this->render('pages/game_result.html.twig');
     }
+
+    #[Route('/hall_of_fame', name: 'app_hall_of_fame')]
+    public function hallOfFame(): Response
+    {
+        return $this->render('pages/hall_of_fame.html.twig');
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('pages/about.html.twig');
+    }
+
+
 }
