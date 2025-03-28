7dfd73adf7a2abd9f0dcf35843f260ed05c33c21
Abdel Bailoul <ab.bailoul@gmail.com>
2025-03-28 23:25:05 +0100

    fix: update name user entity due to reserved word 'user' in pg

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,10 +11,10 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[ORM\Table(name: "`user`")]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
-
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
