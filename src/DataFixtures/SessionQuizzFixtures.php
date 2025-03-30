2f65359f3b86f898a86e08fc85162e6ab1488d50
Abdel Bailoul <ab.bailoul@gmail.com>
2025-03-30 15:03:43 +0200

    refactor: update SessionQuizzFixtures

--- src/DataFixtures/SessionQuizzFixtures.php
+++ src/DataFixtures/SessionQuizzFixtures.php
@@ -16,28 +16,26 @@ class SessionQuizzFixtures extends Fixture implements DependentFixtureInterface
         ObjectManager $manager,
     ): void {
         $faker = Factory::create('fr_FR');
-        // create 50 SessionQuizz
+        // create 50 session quizz
         for ($i = 0; $i < 50; $i++) {
             $sessionQuizz = new SessionQuizz();
-            $sessionQuizz->setStatus($faker->randomElement(['waiting', 'in_progress', 'finished']))
-                ->setMode($faker->randomElement(['solo', 'multi_players']))
-                ->setStartDate(new \DateTimeImmutable($faker->dateTimeBetween('-1 month', '-2 weeks')->format('Y-m-d H:i:s')));
-
-            $startDate = $sessionQuizz->getStartDate();
-
-            $sessionQuizz->setEndDate($startDate->modify('+1 hour'));
 
             $quizz = $this->getReference('quizz_' . $i, Quizz::class);
-            $sessionQuizz->setQuizz($quizz);
-
             $presenter = $this->getReference('user_' . $i, User::class);
-            $sessionQuizz->setPresenter($presenter);
+
+            $startDate = new \DateTimeImmutable($faker->dateTimeBetween('-1 month', '-2 weeks')->format('Y-m-d H:i:s'));
+
+            $sessionQuizz->setStatus($faker->randomElement(['waiting', 'in_progress', 'finished']))
+                ->setMode($faker->randomElement(['solo', 'multi_players']))
+                ->setStartDate($startDate)
+                ->setEndDate($startDate->modify('+1 hour'))
+                ->setQuizz($quizz)
+                ->setPresenter($presenter);
 
             $manager->persist($sessionQuizz);
 
             $this->addReference('sessionQuizz_' . $i, $sessionQuizz);
         }
-
         $manager->flush();
     }
 
